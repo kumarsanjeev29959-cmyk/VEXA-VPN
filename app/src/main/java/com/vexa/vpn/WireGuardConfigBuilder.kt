5bf7e520194c797a466a5a09679b19cedc67cb44
kumarsanjeev29959-cmyk <kumarsanjeev29959@gmail.com>
2026-09-17 13:27:51 +0530

    security: harden generated WireGuard config inputs

--- app/src/main/java/com/vexa/vpn/WireGuardConfigBuilder.kt
+++ app/src/main/java/com/vexa/vpn/WireGuardConfigBuilder.kt
@@ -1,13 +1,25 @@
 package com.vexa.vpn
 
 object WireGuardConfigBuilder {
+    private fun requireSingleLine(name: String, value: String) {
+        require(value.isNotBlank()) { "$name is empty" }
+        require('\n' !in value && '\r' !in value) { "$name contains an invalid line break" }
+    }
+
+    private fun requireTunnelAddress(value: String) {
+        require(Regex("^10\\.64\\.(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.(?:[1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-4])/32$").matches(value)) {
+            "VPN address is invalid"
+        }
+    }
+
     fun build(privateKey: String, response: VpnConfigResponse): String {
-        require(privateKey.isNotBlank()) { "Client private key is empty" }
-        require(response.peer.serverPublicKey.isNotBlank()) { "Server public key is empty" }
-        require(response.peer.address.isNotBlank()) { "VPN address is empty" }
-        require(response.peer.dns.isNotBlank()) { "VPN DNS is empty" }
-        require(response.peer.allowedIPs.isNotBlank()) { "Allowed IPs are empty" }
-        require(response.server.hostname.isNotBlank()) { "VPN server hostname is empty" }
+        requireSingleLine("Client private key", privateKey)
+        requireSingleLine("Server public key", response.peer.serverPublicKey)
+        requireSingleLine("VPN address", response.peer.address)
+        requireTunnelAddress(response.peer.address)
+        requireSingleLine("VPN DNS", response.peer.dns)
+        requireSingleLine("Allowed IPs", response.peer.allowedIPs)
+        requireSingleLine("VPN server hostname", response.server.hostname)
         require(response.server.port in 1..65535) { "VPN server port is invalid" }
         require(response.peer.persistentKeepalive in 0..65535) { "Persistent keepalive is invalid" }
 
